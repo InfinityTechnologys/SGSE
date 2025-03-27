59e6ab853a2936b14fb11cd442c575c7491d67a3
InfinityTech <infinitytech.takimi@gmail.com>
2025-03-27 21:20:25 +0300

    Add files via upload

---
+++ WEB/Script/index.js
@@ -0,0 +1,46 @@
+window.addEventListener("load", function () {
+  setTimeout(function () {
+    document.getElementById("preloader").classList.add("hidden");
+  }, 500);
+});
+
+function go_sgse() {
+  try {
+    window.location.href = "https://sgse.pages.dev/main";
+  } catch (error) {
+    showErrorMessage("Bir hata oluştu! . '" + error.name + "'");
+  }
+}
+
+function showErrorMessage(message) {
+  let errorBox = document.getElementById("errorBox");
+  let errorText = document.getElementById("errorText");
+
+  errorText.innerHTML = message;
+  errorBox.style.display = "block";
+
+  setTimeout(() => {
+    errorBox.style.opacity = "1";
+    errorBox.style.transform = "translateY(0)";
+  }, 10);
+
+  setTimeout(() => {
+    closeError();
+  }, 3000);
+}
+
+function closeError() {
+  let errorBox = document.getElementById("errorBox");
+  errorBox.style.opacity = "0";
+  errorBox.style.transform = "translateY(-10px)";
+
+  setTimeout(() => {
+    errorBox.style.display = "none";
+  }, 500);
+}
+
+function go_sgse_business() {
+  showErrorMessage(
+    "İşletme sitemiz yapım aşamasında olduğu için açılamamaktadır!"
+  );
+}
---
+++ WEB/Script/support.js
@@ -0,0 +1,5 @@
+window.addEventListener("load", function () {
+  setTimeout(function () {
+    document.getElementById("preloader").classList.add("hidden");
+  }, 500);
+});
